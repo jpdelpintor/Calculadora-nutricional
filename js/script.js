20d3dd3429cc4d832d1f43865bc5c679ec55ba8e
João Pedro Ferreira Del Pintor <76700229+jpdelpintor@users.noreply.github.com>
2023-06-09 17:19:47 -0300

    update página de cálculo

--- js/script.js
+++ js/script.js
@@ -15,13 +15,44 @@ function activateMenu (){
 
 botoes.forEach(bot => {
     bot.addEventListener('click', (e) =>{
-        if (e.target.localName === 'span'){
-            
-            console.log('span')
-        } else{
-            const texto = e.target.innerText
-            
-            console.log(texto)
+        
+        const selectedClass = e.target.classList
+
+        if (selectedClass.contains('spanProdutos')) {
+            if (document.querySelector("ul#iproducts").classList.contains("hidedropmenu")){
+                document.querySelector("ul#iproducts").classList = "showdropmenu"
+            } else {
+                document.querySelector("ul#iproducts").classList = "hidedropmenu"
+        
+            }
+        } else if(selectedClass.contains('spanInstitucional')){
+            if (document.querySelector("ul#iinstitutional").classList.contains("hidedropmenu")){
+                document.querySelector("ul#iinstitutional").classList = "showdropmenu"
+            } else {
+                document.querySelector("ul#iinstitutional").classList = "hidedropmenu"
+        
+            }
+        } else if(selectedClass.contains('spanConteudo')){
+            if (document.querySelector("ul#icontent").classList.contains("hidedropmenu")){
+                document.querySelector("ul#icontent").classList = "showdropmenu"
+            } else {
+                document.querySelector("ul#icontent").classList = "hidedropmenu"
+        
+            }
+        } else if(selectedClass.contains('spanMinhaConta')){
+            if (document.querySelector("ul#imyuser").classList.contains("hidedropmenu")){
+                document.querySelector("ul#imyuser").classList = "showdropmenu"
+            } else {
+                document.querySelector("ul#imyuser").classList = "hidedropmenu"
+        
+            }
+        } else {
+            if (document.querySelector("ul#isupport").classList.contains("hidedropmenu")){
+                document.querySelector("ul#isupport").classList = "showdropmenu"
+            } else {
+                document.querySelector("ul#isupport").classList = "hidedropmenu"
+        
+            }
         }
          
          
@@ -29,47 +60,3 @@ botoes.forEach(bot => {
     
 });
 
-function activateDropMenu (e){
-    const texto = e.target.innerText
-
-    if (texto = 'Produtos') {
-        if (document.querySelector("ul#iproducts").classList.contains("hidedropmenu")){
-            document.querySelector("ul#iproducts").classList = "showdropmenu"
-        } else {
-            document.querySelector("ul#iproducts").classList = "hidedropmenu"
-    
-        }
-    } else if(texto = 'Institucional'){
-        if (document.querySelector("ul#iinstitutional").classList.contains("hidedropmenu")){
-            document.querySelector("ul#iinstitutional").classList = "showdropmenu"
-        } else {
-            document.querySelector("ul#iinstitutional").classList = "hidedropmenu"
-    
-        }
-    } else if(texto = 'Conteúdo'){
-        if (document.querySelector("ul#icontent").classList.contains("hidedropmenu")){
-            document.querySelector("ul#icontent").classList = "showdropmenu"
-        } else {
-            document.querySelector("ul#icontent").classList = "hidedropmenu"
-    
-        }
-    } else if(texto = 'Minha Conta'){
-        if (document.querySelector("ul#imyuser").classList.contains("hidedropmenu")){
-            document.querySelector("ul#imyuser").classList = "showdropmenu"
-        } else {
-            document.querySelector("ul#imyuser").classList = "hidedropmenu"
-    
-        }
-    } else {
-        if (document.querySelector("ul#isupport").classList.contains("hidedropmenu")){
-            document.querySelector("ul#isupport").classList = "showdropmenu"
-        } else {
-            document.querySelector("ul#isupport").classList = "hidedropmenu"
-    
-        }
-    }
-    
-    
-    
-    
-}
