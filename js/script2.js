fc7295e0a0a7696a9ed79149ffb0546c7e3eb64c
João Pedro Ferreira Del Pintor <76700229+jpdelpintor@users.noreply.github.com>
2023-06-15 15:09:34 -0300

    update script.js

--- js/script2.js
+++ js/script2.js
@@ -1,3 +1,33 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js'
+import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js'
+import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-analytics.js'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyAHFCzhEr6SpEsvIfruiRe2G5csLhC42oo',
+  authDomain: 'calculadora-nutricional-4d4de.firebaseapp.com',
+  projectId: 'calculadora-nutricional-4d4de',
+  storageBucket: 'calculadora-nutricional-4d4de.appspot.com',
+  messagingSenderId: '284208897892',
+  appId: '1:284208897892:web:d042888b3b63a242276471',
+  measurementId: 'G-CJRPVJPMPE'
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+const analytics = getAnalytics(app)
+
+getDocs(collection(db, 'ingredientes'))
+ .then(querySnapshot => {
+    console.log(querySnapshot)
+ })
+ .catch(console.log)
+
 const teorGordura = document.querySelector('#gordura')
 const teorCarboidrato = document.querySelector('#carboidrato')
 const teorProteina = document.querySelector('#proteina')
@@ -52,7 +82,7 @@ class calculadora{
 
 const calc = new calculadora
 
-botaoInserir.addEventListener("click", ()=>{
+botaoInserir.addEventListener('click', ()=>{
     
     calc.addDemandas()
 
@@ -60,7 +90,7 @@ botaoInserir.addEventListener("click", ()=>{
     console.log(`O valor é ${calc.teorCarboidrato} e é do tipo ${typeof(calc.teorCarboidrato)}`)
 })
 
-botaoAdicionar.addEventListener("click", ()=>{
+botaoAdicionar.addEventListener('click', ()=>{
     
     calc.addIngredientes()
 
